bef71e35d215b86aa7745fa99c8157febd2ad0c0
Nick Campbell <nicholas.j.campbell@gmail.com>
2012-05-11 01:00:58 -0400

    pathing
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- custom/path.zsh
+++ custom/path.zsh
@@ -1,1 +1,1 @@
-export PATH=~/Code/depot_tools:$PATH:~/bin/:/opt/eclipse/
+export PATH=~/Code/scripts:~/Code/depot_tools:$PATH:~/bin/:/opt/eclipse/:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools
