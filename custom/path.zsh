4ff0a5f8ea42d720c3ba40c288c9ca7b927c6882
Nick Campbell <nicholas.j.campbell@gmail.com>
2013-04-19 10:53:53 -0400

    Go binaries
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- custom/path.zsh
+++ custom/path.zsh
@@ -1,1 +1,1 @@
-export PATH=~/Code/scripts:~/Code/depot_tools:$PATH:~/bin:~/sbin:/opt/eclipse:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools:/usr/local/scala/bin
+export PATH=~/Code/scripts:~/Code/depot_tools:$PATH:~/bin/:~/sbin/:/opt/eclipse/:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools:/usr/local/scala/bin/:/usr/local/go/bin
