b2e09859612d6a0d46687b3d47a913b1a3aee38d
Nick Campbell <nicholas.j.campbell@gmail.com>
2014-06-19 09:01:35 -0400

    Updated path
    Signed-off-by: Nick Campbell <nicholas.j.campbell@gmail.com>

--- custom/path.zsh
+++ custom/path.zsh
@@ -1,1 +1,1 @@
-export PATH=~/Code/scripts:~/Code/depot_tools:$PATH:~/bin/:~/sbin/:/opt/eclipse/:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools:/usr/local/scala/bin/:/usr/local/go/bin
+export PATH=/usr/local/opt/coreutils/libexec/gnubin:~/Code/scripts:~/Code/depot_tools:$PATH:~/bin/:~/sbin/:/opt/eclipse/:/opt/android-sdk-linux/tools:/opt/android-sdk-linux/platform-tools:/usr/local/scala/bin/:/usr/local/go/bin
